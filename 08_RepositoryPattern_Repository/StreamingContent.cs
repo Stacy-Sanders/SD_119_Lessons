304adadc5517f084bbc0fcec44b6360ed1d4ad07
Stacy-Sanders <stacy.sanders081@gmail.com>
2021-12-14 11:43:30 -0500

    RP Repository

---
+++ 08_RepositoryPattern_Repository/StreamingContent.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_RepositoryPattern_Repository
+{
+    public enum GenreType
+    {
+        Horror = 1,
+        RomCom,
+        SciFi,
+        Documentary,
+        Bromance,
+        Drama,
+        Action
+    }
+
+    public enum MaturityRating
+    {
+        G,
+        PG,
+        PG_13,
+        R,
+        NC_17,
+        TV_Y,
+        TV_G,
+        TV_PG,
+        TV_14,
+        TV_MA
+    }
+    public class StreamingContent
+    {
+        public StreamingContent() { }
+
+        public StreamingContent
+            (string title,
+            string description,
+            double starRating,
+            MaturityRating maturityRating,
+            GenreType typeOfGenre)
+        {
+            Title = title;
+            Description = description;
+            StarRating = starRating;
+            MaturityRating = maturityRating;
+            TypeOfGenre = typeOfGenre;
+        }
+        // Unique identifier
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public double StarRating { get; set; }
+        public MaturityRating MaturityRating { get; set; }
+        public GenreType TypeOfGenre { get; set; }
+        public bool IsFamilyFriendly
+        {
+            get
+            {
+                switch (MaturityRating)
+                {
+                    case MaturityRating.G:
+                    case MaturityRating.PG:
+                    case MaturityRating.TV_Y:
+                    case MaturityRating.TV_G:
+                    case MaturityRating.TV_PG:
+                        return true;
+                    case MaturityRating.R:
+                    case MaturityRating.PG_13:
+                    case MaturityRating.TV_14:
+                    case MaturityRating.NC_17:
+                    case MaturityRating.TV_MA:
+                        return false;
+                    default:
+                        return false;
+                }
+
+
+                // Here the order of the enum matters (ctrl K C to change to comment)
+                //if ((int)MaturityRating >4)
+                //{
+                //    return false;
+                //}
+                //else
+                //{
+                //    return true;
+                //}
+
+
+
+
+
+
+            }
+
+
+            /*
+                Users have been complaining about the family friendly content.
+                Some users have been reporting that when filtering for family friendly,
+                they're getting some content with inappropriate maturity ratings. We need to fix this.
+                Currently the maturity rating and famliy friendly bool are independent, we need to tie them together.
+                If something is rated R (or another similar rating), it should never be able to have a IsFamilyFriendly of true.
+
+                We need you to refactor the code StreamingContent class. To help narrow down our problem, we want to replace the 
+                MaturityRating with a default set of options. Based on those options, we want our IsFamilyFriendly to return the 
+                appropriate true or false.
+
+            */
+        }
+    }
+}
+
---
+++ 08_RepositoryPattern_Repository/StreamingContentRepository.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08_RepositoryPattern_Repository
+{
+    // This is our fake database
+    public class StreamingContentRepository
+    {
+        // private means only accessible in this class
+        // protected is like private+ - it includes derived/child/inherited classes
+        protected readonly List<StreamingContent> _contentDirectory = new List<StreamingContent>();
+
+        // Create
+        public bool AddContentToDirectory(StreamingContent content)
+        {
+            int startingCount = _contentDirectory.Count;
+            _contentDirectory.Add(content);
+
+            // we need to check to see if this works
+            bool wasAdded = (_contentDirectory.Count > startingCount) ? true : false;
+            return wasAdded;
+        }
+        // Read
+        public List<StreamingContent> GetAllContents()
+        {
+            return _contentDirectory;
+        }
+        public List<StreamingContent> GetAllContentByGenre(GenreType genreType)
+        {
+            // make a variable that will hold all of the "found" items
+            List<StreamingContent> foundContent = new List<StreamingContent>();
+            foreach (StreamingContent content in _contentDirectory)
+            {
+                if (content.TypeOfGenre == genreType)
+                {
+                    foundContent.Add(content);
+                }
+            }
+            return foundContent;
+
+            // return _contentDirectory.Where(cd => cd.TypeofGenre == genreType).ToList();
+        }
+
+        // CHALLENGE:
+        // Make a GetFamilyFriendlyContents() method
+
+        public List<StreamingContent> GetAllFamilyFriendlyContents()
+        {
+            List<StreamingContent> foundContent = new List<StreamingContent>();
+            foreach (StreamingContent content in _contentDirectory)
+            {
+                if (content.IsFamilyFriendly == true)
+                {
+                    foundContent.Add(content);
+                }
+            }
+            return foundContent;
+        }
+
+
+
+        // Andrew's answer
+
+        public List<StreamingContent> GetFamilyFriendlyContents()
+        {
+            List<StreamingContent> familyFriendlyContents = new List<StreamingContent>();
+            foreach (StreamingContent content in _contentDirectory)
+            {
+                if (content.IsFamilyFriendly)
+                {
+                    familyFriendlyContents.Add(content);
+                }
+            }
+            return familyFriendlyContents;
+        }
+
+
+
+
+
+
+
+
+            // Read(unique identifier)
+            // Helper method
+            public StreamingContent GetContentByTitle(string title)
+            {
+                foreach (StreamingContent content in _contentDirectory)
+                {
+                    if (content.Title.ToLower() == title.ToLower())
+                    {
+                        return content;
+                    }
+                }
+                // If it gets here and hasn't found anything...
+                return null;
+
+                // return _contentDirectory.FirstOrDefault(sc => sc.Title == title);
+            }
+            // Update
+            public bool UpdateExistingContent(string originalTitle, StreamingContent content)
+            {
+                // find the old content...
+                StreamingContent oldContent = GetContentByTitle(originalTitle);
+
+                if (oldContent != null)
+                {
+                    oldContent.Title = content.Title;
+                    oldContent.Description = content.Description;
+                    oldContent.StarRating = content.StarRating;
+                    oldContent.MaturityRating = content.MaturityRating;
+                    oldContent.TypeOfGenre = content.TypeOfGenre;
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            // Delete
+            public bool DeleteExistingContent(StreamingContent existingContent)
+            {
+                bool deleteResult = _contentDirectory.Remove(existingContent);
+                return deleteResult;
+            }
+    }
+}
+
+
+
+
+
+       
